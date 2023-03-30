dc2e53c879d9fa267043319ce74672d3192e49e5
liuyk <liuyk@tsingyun.net>
2023-03-30 11:55:34 +0800

    init

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const pm2 = require('pm2')
+const pmx = require('pmx')
 
-pm2.launchBus(function(err, pm2_bus) {
-    pm2_bus.on('process:msg', function(packet) {
-        console.log(packet)
+pmx.initModule(
+    // 配置
+    {},
+    // 函数
+    function (err, conf) {
+        if (err) {
+            throw err;
+        }
+        // 当前模块的ID
+        let cPmId = -1;
+        // 查询当前进程
+        pm2.list((err, list) => {
+            if (err) throw err;
+            for (let item of list) {
+                if (item.name === conf.module_name) {
+                    cPmId = item.pm_id;
+                }
+            }
+        });
+
+        pm2.launchBus(function (err, bus) {
+            bus.on('*', function (packet) {
+                console.log(packet)
+            })
+        });
     })
-})
